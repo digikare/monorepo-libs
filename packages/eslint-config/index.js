1af4db27addd0d86ac20e3a2bcef86dddfa091f5
Tieu-Philippe KHIM <khim.tieu.philippe@gmail.com>
2020-10-29 18:58:19 +0100

    build(lint): refactor eslint to his own package

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+    ecmaVersion: 2019,
+  },
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    browser: true,
+  },
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-types": "warn",
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  },
+};
